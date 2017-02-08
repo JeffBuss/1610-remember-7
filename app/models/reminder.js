fd99cb7083483bc1e7160f111b3f5ddf8bab5fe4
JeffBuss <turbocapslock@gmail.com>
2017-02-08 16:53:01 -0700

    Reminders are displaying on page

--- app/models/reminder.js
+++ app/models/reminder.js
@@ -4,5 +4,5 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   date: DS.attr('string'),
   notes: DS.attr('string'),
-  pinned: DS.attr('boolean', { defaultValue:false })
+  pinned: DS.attr('boolean', { defaultValue: false })
 });
