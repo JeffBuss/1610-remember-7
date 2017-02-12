3b510c72d98e1c72dba670477c68435097befce0
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-02-11 19:20:08 -0700

    Add toggleDisplay action and active property

--- app/components/reminder-item.js
+++ app/components/reminder-item.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isActive: false,
+  actions: {
+    toggleDisplay() {
+      this.toggleProperty('isActive')
+    }
+  }
 });
 
 // classNames: ['spec-reminder-item'],
