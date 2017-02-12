d7ac460be5b7993ae520c992a024a3554c9824dc
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-02-11 19:20:08 -0700

    Fix CPing conflicts, and move new note button to bottom of list b/c specs

--- app/components/reminder-item.js
+++ app/components/reminder-item.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isActive: false,
+  actions: {
+    toggleDisplay() {
+      this.toggleProperty('isActive')
+    }
+  }
 });
