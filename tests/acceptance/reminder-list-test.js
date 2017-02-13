b678fa008aaae8e085a62d2f2838102ee9526929
JeffBuss <turbocapslock@gmail.com>
2017-02-13 12:25:50 -0700

    Fix CPing issues in tests

--- tests/acceptance/reminder-list-test.js
+++ tests/acceptance/reminder-list-test.js
@@ -27,7 +27,7 @@ test('clicking on an individual item', function(assert) {
   andThen(function() {
     assert.equal(currentURL(), '/reminders/1', 'should redirect to the first item');
     assert.equal(Ember.$('.spec-reminder-title:first').text().trim(), Ember.$('.spec-reminder-title:first').text().trim(), 'should find the title of the reminder item');
-    assert.equal(Ember.$('.active').length, 2, 'the reminder clicked has an active class')
+    assert.equal(Ember.$('.active').length, 1, 'the reminder clicked has an active class')
   });
 });
 
