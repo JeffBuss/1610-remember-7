89bb030debdc40eb12fed4c1cb11dc427973f11c
JeffBuss <turbocapslock@gmail.com>
2017-02-12 21:54:23 -0700

    Basic display note tests passing

--- tests/acceptance/reminder-list-test.js
+++ tests/acceptance/reminder-list-test.js
@@ -30,3 +30,30 @@ test('clicking on an individual item', function(assert) {
     assert.equal(Ember.$('.active').length, 2, 'the reminder clicked has an active class')
   });
 });
+
+test('clicking new note button', function(assert) {
+  visit('/');
+  click('button');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/reminders/new', 'should redirect to add a new item');
+    assert.equal(find('input').text(), '', "the input fields are empty" );
+  })
+})
+
+test('adding new notes', function(assert) {
+  visit('/reminders/new')
+
+  andThen(function() {
+    fillIn('.spec-title-input', 'Stay Thirsty');
+    // fillIn('.spec-date-input', '2017-2-2');
+    // fillIn('.spec-note-input', 'Brenna owes me an organic gatorade');
+
+    click('.spec-submit-new-btn');
+
+    andThen(function() {
+      assert.equal(find('.spec-reminder-item').length, 1, 'should append a new reminder to the page when clicked')
+      assert.equal(find('.spec-reminder-title').text(), 'Stay Thirsty', 'should display the correct title')
+    })
+  })
+})
