eae68cf9ad15791a63ef27fae17bab04e88554d8
JeffBuss <turbocapslock@gmail.com>
2017-02-14 19:20:49 -0700

    Save button now returns user to reminders/:id

--- app/components/edit-reminder.js
+++ app/components/edit-reminder.js
@@ -3,15 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
 
-  actions:  {
-    editReminder() {
-      console.log("Button does thing!");
-    },
+  actions: {
     saveReminder() {
-      const reminder = this.getProperties('title', 'date', 'notes');
-      console.log(reminder)
-      this.set('reminder', reminder).save().then(() => {
-        this.transitionTo('/reminders')
+      this.get('store')
+      .findRecord('reminder', this.model.id)
+      .then(reminder => {
+        reminder.save()
       })
     }
   }
