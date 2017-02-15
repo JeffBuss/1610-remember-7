200ddffb8457abdcec14be703014436bb062dcea
JeffBuss <turbocapslock@gmail.com>
2017-02-15 13:34:42 -0700

    Edit and save note functionality now complete

--- app/routes/edit.js
+++ app/routes/edit.js
@@ -1,9 +1,14 @@
-// import Ember from 'ember';
-//
-// export default Ember.Route.extend({
-//   action: {
-//     saveReminder() {
-//       this.transitionTo("/reminders")
-//     }
-//   }
-// });
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  model() {
+    return this.get('store').findRecord('reminder', this.model.id);
+  },
+
+  action: {
+    saveReminder() {
+      this.transitionTo("/reminders")
+    }
+  }
+});
