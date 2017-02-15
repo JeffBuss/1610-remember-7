604d179454e45a82c3f7b9a3b0f30ae706c3c3f7
Jeff Buss <turbocapslock@gmail.com>
2017-02-15 15:14:15 -0700

    Delete unnecessary action
    This is now being handled by a link in the edit-reminder component.

--- app/routes/edit.js
+++ app/routes/edit.js
@@ -5,10 +5,4 @@ export default Ember.Route.extend({
   model() {
     return this.get('store').findRecord('reminder', this.model.id);
   },
-
-  action: {
-    saveReminder() {
-      this.transitionTo("/reminders")
-    }
-  }
 });
